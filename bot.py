bc0e62f5bdd3dba9a26af862628e8e89c4e6bd15
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-02-03 17:59:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,4 +125,4 @@ if __name__ == '__main__':
 #    try:
   #      loop.run_until_complete(start())
   #  except KeyboardInterrupt:
-    ₹    logging.info('Service Stopped Bye 👋')
+  #      logging.info('Service Stopped Bye 👋')
